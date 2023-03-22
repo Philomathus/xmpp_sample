fe736bc9b39d2779b3f2b2446b7e5f2ca19147ff
krzystof <krzystof.wbridges@gmail.com>
2023-03-22 18:16:55 +0800

    Initial commit

---
+++ krzystof.js
@@ -0,0 +1,26 @@
+const xmpp = require( "simple-xmpp" )
+
+xmpp.connect( {
+    "jid": "krzystof@localhost",
+    "password": "123abc",
+    "host": "localhost",
+    "port": 5222
+} )
+
+xmpp.on( "online", data => {
+    console.log( "You are online" )
+    send()
+} )
+
+xmpp.on( "error", error => {
+    console.log( "Something went wrong" )
+} )
+
+xmpp.on( "chat", ( from, message ) => {
+    console.log(`Received: ${message} from ${from}`)
+})
+
+function send() {
+    setTimeout( send, 5000);
+    xmpp.send("admin@localhost", "Hi!");
+}
